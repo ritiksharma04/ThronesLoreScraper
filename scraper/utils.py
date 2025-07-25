3a9e4e99c1196dbe2849c59a2b0e7272705a5e1c
ritiksharma04 <ritikmgmt@gmail.com>
2025-07-25 13:12:57 +0530

    fix bugs

--- scraper/utils.py
+++ scraper/utils.py
@@ -1,10 +1,15 @@
 import csv
+import os
+
 def save_to_csv(data, filepath):
     if not data:
             print("no data")
             return
-    headers = data[0].keys()
+
+    #making data directory
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
     
+    headers = data[0].keys()
     with open(filepath, 'w', newline='', encoding='utf-8') as f:
         d = csv.DictWriter(f,fieldnames=headers)
         d.writeheader()
